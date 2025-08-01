77ded0a52472fb3b212b1d7dff713db6a351d78a
carloshenrique-dd <carlos.alves.ca12@gmail.com>
2025-08-01 15:32:27 -0300

    feat: profile test in env validation

--- src/shared/env/index.ts
+++ src/shared/env/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config"
 import z from "zod"
 
 const envSchema = z.object({
-	NODE_ENV: z.enum(["DEV", "PROD"]),
+	NODE_ENV: z.enum(["DEV", "PROD", "TEST"]),
 	PORT: z.coerce.number().min(1).max(65535),
 	DATABASE_URL: z.string()
 })
@@ -12,7 +12,11 @@ const _env = envSchema.safeParse(process.env)
 if (!_env.success) {
 	console.error("❌ Erro na validacao das variaveis de ambiente")
 	console.error(_env.error.issues)
-	process.exit(1)
+	if (process.env.NODE_ENV !== "TEST") {
+		process.exit(1)
+	} else {
+		throw new Error("Variáveis de ambiente inválidas para o ambiente de teste.")
+	}
 }
 
 const env = _env.data
