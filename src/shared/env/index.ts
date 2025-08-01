4d5837533de145d1c5f2e1f157c398f582834373
carloshenrique-dd <carlos.alves.ca12@gmail.com>
2025-08-01 13:13:03 -0300

    feat: env validation

---
+++ src/shared/env/index.ts
@@ -0,0 +1,20 @@
+import "dotenv/config"
+import z from "zod"
+
+const envSchema = z.object({
+    NODE_ENV: z.enum(["DEV", "PROD"]),
+    PORT: z.coerce.number().min(1).max(65535),
+    DATABASE_URL: z.string()
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+    console.error("Erro na validacao das variaveis de ambiente")
+    console.error(_env.error.format)
+    process.exit(1)
+}
+
+const env = _env.data
+
+export default env
