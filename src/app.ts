9134141eac3cd237c9250e4f60e7e2003a47d255
carloshenrique-dd <carlos.alves.ca12@gmail.com>
2025-08-01 14:13:49 -0300

    feat: global error middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cors from "cors"
 import Express from "express"
 import helmet from "helmet"
 import morgan from "morgan"
+import errorHandler from "./shared/middlewares/errorHandler"
 
 const app = Express()
 
@@ -9,5 +10,7 @@ app.use(morgan("combined"))
 app.use(helmet)
 app.use(cors)
 app.use(Express.json())
+
+app.use(errorHandler)
 
 export default app
---
+++ src/shared/middlewares/errorHandler.ts
@@ -0,0 +1,22 @@
+import type { ErrorRequestHandler } from "express"
+import env from "../env"
+import { HttpError } from "../errors/HttpError"
+
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+	const isHttpError = err instanceof HttpError
+
+	const statusCode = isHttpError ? err.statusCode : 500
+	const message = isHttpError ? err.message : "internal server error"
+
+	const responseBody: Record<string, any> = {
+		message
+	}
+
+	if (env.NODE_ENV === "DEV" && statusCode === 500) {
+		responseBody.stack = err.stack
+	}
+
+	res.status(statusCode).json(responseBody)
+}
+
+export default errorHandler
