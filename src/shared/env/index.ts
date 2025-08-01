8e895e195bac9dd5ce96c0a03794c1ca56224586
carloshenrique-dd <carlos.alves.ca12@gmail.com>
2025-08-01 14:13:07 -0300

    fix: env validation, start script

--- src/shared/env/index.ts
+++ src/shared/env/index.ts
@@ -2,17 +2,17 @@ import "dotenv/config"
 import z from "zod"
 
 const envSchema = z.object({
-    NODE_ENV: z.enum(["DEV", "PROD"]),
-    PORT: z.coerce.number().min(1).max(65535),
-    DATABASE_URL: z.string()
+	NODE_ENV: z.enum(["DEV", "PROD"]),
+	PORT: z.coerce.number().min(1).max(65535),
+	DATABASE_URL: z.string()
 })
 
 const _env = envSchema.safeParse(process.env)
 
 if (!_env.success) {
-    console.error("Erro na validacao das variaveis de ambiente")
-    console.error(_env.error.format)
-    process.exit(1)
+	console.error("❌ Erro na validacao das variaveis de ambiente")
+	console.error(_env.error.issues)
+	process.exit(1)
 }
 
 const env = _env.data
